879a716beeaed75b23d56e59942d4442fb14c1d0
JrainLau <jrainlau@163.com>
2017-05-23 20:09:45 +0800

    第一课时

---
+++ List和Tuple类型/demo-1.py
@@ -0,0 +1,9 @@
+L = ['Adam', 'Lisa', 'Bart']
+L.append('Jrain')
+L.insert(1, 'Paul')
+L.pop(-2)
+print L
+
+T = ('Adam', ['Lisa', 'Bart'])
+T[1][0] = 'Zoey'
+print T
---
+++ 变量和数据类型/demo-1.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+print 45678 + 0x12fd2
+print 100 < 99
+print 0xff == 255
+print 'hello,' , 'python'
+print 'hello' + ', python'
+print 'hello, python'
+print r'''
+1
+2
+3'''
+
+print '中文字符'
+print u'中文字符'
